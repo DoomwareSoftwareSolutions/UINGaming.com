954f90c3d2403da07bada4f12ad3f2fbdc7f3699
tomasboccardo <tomasboccardo@gmail.com>
2013-09-11 23:50:00 -0300

    Saco unas lineas que use para debug

--- UINGaming/static/js/controllers/SignInCtrl.js
+++ UINGaming/static/js/controllers/SignInCtrl.js
@@ -8,10 +8,5 @@ angular.module(controllerName, []).
     controller(controllerName, ['$scope', '$http', '$q', 'AuthService',  function ($scope, $http, $q, AuthService) {
 
         $scope.$emit("BackgroundChange", "signin-background");
-	
-	/* Eso es solo de prueba. Hay que sacarlo */
-	
-	var userData = {username: "tomas", password: "1234", remember: true}
-	AuthService.loginUser($q, $scope, userData);
 
     }]);
